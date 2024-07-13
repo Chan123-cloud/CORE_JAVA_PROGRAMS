b47b8a5484864bddefd286ca9a095298cdadfda7
Chan123-cloud <144804619+Chan123-cloud@users.noreply.github.com>
2024-07-13 16:34:33 +0530

    Add files via upload

---
+++ Final/Cls.java
@@ -0,0 +1,13 @@
+package Final;
+
+public class Cls extends FinalMethods {   // Here Inheritance has Achieved but over ridden can't achieved.
+	
+	public static void main(String[] args) {
+		
+		Cls a = new Cls();
+		
+		System.out.println(a.print());
+			
+	}
+
+}
---
+++ Final/FinalMethods.java
@@ -0,0 +1,15 @@
+package Final;
+
+public class FinalMethods {
+	
+	
+	final short a = 90;   // Values are constant Can't change the value
+	
+	
+	 final boolean  print() {  // final methods can't be overridden because they are constant.
+		
+		return false;
+		
+	}
+
+}
