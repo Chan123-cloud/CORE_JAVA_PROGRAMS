cda3e063b04d718a35721d27ea6b364b2b8b82b9
Chan123-cloud <144804619+Chan123-cloud@users.noreply.github.com>
2024-07-17 08:44:08 +0530

    Add files via upload

---
+++ String/StringSwap.java
@@ -0,0 +1,56 @@
+package String;
+
+public class StringSwap {
+	
+	public static void main(String[] args) {
+		
+		String a = "chandra";
+		
+		String b = "Kumar";
+		
+		String c ="";
+		
+		c = a;
+		
+		a = b;
+		
+		b = c;
+		
+		System.out.println(a);  // Using the third variable.
+		
+		System.out.println(b);
+		
+	
+		
+		
+		
+		
+		
+	    a = "chandra";
+	    
+	    b = "kumar";
+	    
+	    
+	    a = a+b;
+	    
+	    System.out.println(a);
+	    
+	    
+	    
+	    b = a.substring( 0, a.length()  - b.length());  // without the third variable using the subset of a string.
+	    
+	    
+	    System.out.println(b);
+	    
+	    
+	    a = a.substring(b.length());
+	    
+	    
+	    System.out.println(a);
+	    
+	  
+		
+	 
+	}
+
+}
