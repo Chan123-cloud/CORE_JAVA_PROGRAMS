39c9299c04224e9dd45b0459bde7ffe76599c662
CHANDRA KUMAR M <144804619+Chan123-cloud@users.noreply.github.com>
2024-07-29 10:18:08 +0530

    Add files via upload

---
+++ Throw_In_Exception_Handling/A1.java
@@ -0,0 +1,13 @@
+package Throw_In_Exception_Handling;
+
+public class A1 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("THis one will Execute");
+		
+		throw new ClassCastException("Exception is created"); // I created the Exception here without any class cast logic
+		
+	}
+
+}
